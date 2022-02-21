addb1b51741452b233682e8fff1c814252563d66
rr107 <raihanratul60@gmail.com>
2022-02-21 15:20:07 +0600

    Laravel Project

---
+++ personalportfolio/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Backend\BackendController;
+use App\Http\Controllers\Backend\BannerController;
+use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [FrontendController::class, "index"])->name('frontend.home');
+
+Auth::routes();
+
+Route::name('backend.')->group(function(){
+    Route::get('/dashboard', [BackendController::class, 'index'])->name('home');
+    Route::resource('/banner', BannerController::class)->except(['show']);    
+});
+
